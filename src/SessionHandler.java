61e6f552b9a900c388d452a5caf362616cc93221
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-11-12 12:45:37 -0800

    updated run method in sessionhandler class to send msgs to client class

--- src/SessionHandler.java
+++ src/SessionHandler.java
@@ -3,6 +3,7 @@ import java.net.*;
 
 public class SessionHandler implements Runnable{
 	private Socket clientSocket;
+	private String connectionStatus = "Failed";
 	
 	public SessionHandler(Socket socket) {
 		this.clientSocket = socket;
@@ -10,6 +11,28 @@ public class SessionHandler implements Runnable{
 	
 	@Override
 	public void run() {
-		try ()
+		try (
+				ObjectInputStream input = new ObjectInputStream(clientSocket.getInputStream());
+				ObjectOutputStream output = new ObjectOutputStream(clientSocket.getOutputStream());
+			) {
+			
+			Object recievedObj = input.readObject();
+			Message message = (Message) recievedObj;
+			
+			if (message.getType().equals("login")) {
+				this.connectionStatus = "Success";
+				
+				output.writeObject(connectionStatus);
+				output.flush();
+				
+				while (message.getType().equals("login")) {
+					
+				}
+			}
+			
+			
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
 	}
 }
