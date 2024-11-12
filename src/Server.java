da3e544b97d1a9f6e7641d5c719fe8e5f8828efc
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-11-12 11:32:18 -0800

    adding template code to server class

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,53 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket; 
+import java.util.List;
+
 
 public class Server {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException{
 		// TODO Auto-generated method stub
-
+		ServerSocket ss = new ServerSocket(7777);
+		System.out.println("ServerSocket awaiting connections...");
+		
+		Socket socket = ss.accept();
+		System.out.println("Connection from " + socket + "!");
+		
+		InputStream inputStream = socket.getInputStream();
+		
+		ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+		
+		List<Message> listOfMessages = (List<Message>) ObjectInputStream.readObject();
+		System.out.println("Recieved [" + listOfMessages.size() + "] messages from: " + socket);
+		
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
