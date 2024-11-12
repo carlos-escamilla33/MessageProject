c8e59523fd0b1f8ffa981021e75673bb76b893c8
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-11-12 12:09:28 -0800

    created SessionThread class to handle incomming client connections

--- src/Server.java
+++ src/Server.java
@@ -3,37 +3,32 @@ import java.net.ServerSocket;
 import java.net.Socket; 
 import java.util.List;
 
-
 public class Server {
+	private static final int PORT = 7777;
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException{
 		// TODO Auto-generated method stub
-		ServerSocket ss = new ServerSocket(7777);
-		System.out.println("ServerSocket awaiting connections...");
 		
-		Socket socket = ss.accept();
-		System.out.println("Connection from " + socket + "!");
 		
-		InputStream inputStream = socket.getInputStream();
-		
-		ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-		
-		List<Message> listOfMessages = (List<Message>) ObjectInputStream.readObject();
-		System.out.println("Recieved [" + listOfMessages.size() + "] messages from: " + socket);
-		
-		System.out.println("All messages: ");
-		listOfMessages.forEach(msg -> printMessage(msg));
-		
-		System.out.println("Closing sockets.");
-		ss.close();
-		socket.close();
-	}
-	
-	private static void printMessage(Message msg) {
-		System.out.println("ID: " + msg.getId());
-		System.out.println("Satus: " + msg.getStatus());
-		System.out.println("Type: " + msg.getType());
-		System.out.println("Text: " + msg.getText());
+//		ServerSocket ss = new ServerSocket(PORT);
+//		System.out.println("ServerSocket awaiting connections...");
+//		
+//		Socket socket = ss.accept();
+//		System.out.println("Connection from " + socket + "!");
+//		
+//		InputStream inputStream = socket.getInputStream();
+//		
+//		ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+//		
+//		List<Message> listOfMessages = (List<Message>) ObjectInputStream.readObject();
+//		System.out.println("Recieved [" + listOfMessages.size() + "] messages from: " + socket);
+//		
+//		System.out.println("All messages: ");
+//		listOfMessages.forEach(msg -> printMessage(msg));
+//		
+//		System.out.println("Closing sockets.");
+//		ss.close();
+//		socket.close();
 	}
 
 }
---
+++ src/SessionThread.java
@@ -0,0 +1,4 @@
+
+public class SessionThread implements Runnable{
+	
+}
